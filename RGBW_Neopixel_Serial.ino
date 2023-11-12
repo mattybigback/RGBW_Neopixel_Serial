34631d1c7e2985e621d2acb88b573b0f0887d3c5
Matt Harrison <mattybigback@gmail.com>
2023-11-12 20:57:58 +0000

    Start byte and checksum working

--- RGBW_Neopixel_Serial.ino
+++ RGBW_Neopixel_Serial.ino
@@ -1,4 +1,3 @@
-
 /*
 Control a single RGBW Neopixel by sending 4 byte strings over serial
 
@@ -6,17 +5,18 @@ Author: Matt Harrison
 Repo: http://github.com/mattybigback/RGBW_Neopixel_Serial
 
 */
+#include <Fletcher16.h>  //v0.1.8 - https://github.com/RobTillaart/Fletcher
 #include <NeoPixelBus.h> //v2.7.5 - https://github.com/Makuna/NeoPixelBus
 
 #define NUM_LEDS 1
-#define NEO_PIN  3
+#define NEO_PIN 3
 
 NeoPixelBus<NeoGrbwFeature, NeoWs2812xMethod> pixel(NUM_LEDS, NEO_PIN);
 
-void setup()
-{
-    Serial.begin(115200);
-    while (!Serial);    // wait for serial
+void setup() {
+    Serial.begin(9600);
+    while (!Serial)
+        ; // wait for serial
 
     Serial.setTimeout(30);
 
@@ -28,27 +28,56 @@ void setup()
     Serial.println("Ready to receive commands");
 }
 
-void loop()
-{
+uint8_t *receive_serial_packet() {
+    static uint8_t serial_instruction[7] = {0, 0, 0, 0, 0, 0, 0};
+    while (Serial.available() > 0) {
+        size_t ser_buffer = Serial.readBytes(serial_instruction, 7);
 
-    if (Serial.available())
-    {
-        uint8_t *rgbw;
-        rgbw = serial_rgbw();
-        pixel.SetPixelColor(0, RgbwColor(rgbw[0], rgbw[1], rgbw[2], rgbw[3]));
-        pixel.Show();
-        for(uint8_t i=0; i<4; i++){
-            Serial.println(rgbw[i]);
+        // Grab checksum from received packet
+        uint16_t received_checksum = serial_instruction[6] | serial_instruction[5] << 8;
+
+        // Initialise checksum
+        Fletcher16 checksum;
+        checksum.begin();
+
+        // Add bytes to be checked by checksum
+        for (uint8_t i = 0; i < 5; i++) {
+            checksum.add(serial_instruction[i]);
+        }
+
+        uint16_t calculated_checksum = checksum.getFletcher();
+
+        if (serial_instruction[0] == 0x7f && calculated_checksum == received_checksum) {
+            Serial.println("valid message!");
+            return serial_instruction;
+        } else {
+            serial_instruction[0] = 0xff;
+            return serial_instruction;
         }
     }
-
 }
 
-uint8_t * serial_rgbw() {
-    static uint8_t serial_instruction[4] = {0, 0, 0, 0};
-    while (Serial.available() > 0)
-    {
-        size_t ser_buffer = Serial.readBytes(serial_instruction, 4);
+void set_LEDs(uint8_t packet[]) {
+    Serial.println("setting LED");
+    for (uint8_t i = 1; i < 5; i++) {
+        Serial.println(packet[i], HEX);
     }
-    return serial_instruction;
+    pixel.SetPixelColor(0, RgbwColor(packet[1], packet[2], packet[3], packet[4]));
+    pixel.Show();
+}
+
+void loop() {
+    if (Serial.available()) {
+        uint8_t *serial_packet;
+        serial_packet = receive_serial_packet();
+
+        if (serial_packet[0] != 0xff) {
+            set_LEDs(serial_packet);
+        }
+
+        // clear buffer
+        for(uint8_t i=0;i<7;i++){
+            serial_packet[i] = 0;
+        }
+    }
 }
